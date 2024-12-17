06bb0b4f66a2dd4368d4ca8aea325224291423fa
amondal2 <ab59@cdc.gov>
2024-12-17 18:59:21 +0000

    function app boilerplate

---
+++ function_app.py
@@ -0,0 +1,13 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+logger = logging.getLogger(__name__)
+
+@app.route("hello-world", methods=["GET"])
+def hello_world(req: func.HttpRequest) -> func.HttpResponse:
+    logger.info("Python HTTP trigger function processed a request.")
+    return func.HttpResponse(
+        "Hello, World!",
+        status_code=200
+    )  
