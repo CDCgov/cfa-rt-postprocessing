ebb6b23d37fdf96066639a2b0aa32607bb0ee2a2
Agastya Mondal, PhD <amondal13@gmail.com>
2025-01-10 14:03:27 -0800

    Merge tasks route + Deploy action (#21)
    * move storage constants to azure utils
    
    * typo in base route
    
    * add validation logic, error handling, merge route
    
    * add tzinfo to arg extract
    
    * delete local directory after upload
    
    * add deploy action
    
    * use defaultazurecredential

--- postprocessing_server/app.py
+++ postprocessing_server/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, Response, request
+
+from utils.main_functions import merge_task_files, validate_args
 
 app = Flask(__name__)
 
@@ -9,4 +11,28 @@ def base():
     Returns:
         str: Startup message.
     """
-    return "<p>Rt postprocessing validation server running.</p>"
+    return "<p>Rt postprocessing server running.</p>"
+
+
+@app.post("/merge")
+def merge():
+    """Route to merge Rt output files.
+    Takes inputs (from the request body):
+        - release_name
+        - min_runat
+        - max_runat
+        - (Optional) rt_output_container_name
+        - (Optional) post_process_container_name
+        - (Optional) overwrite_blobs
+    Returns:
+        flask.Response: JSON response with status and message.
+    """
+    try:
+        validated_args = validate_args(request.json)
+        merge_task_files(**validated_args)
+        response_message = f"Task files successfully merged at {validated_args['post_process_container_name']}/{validated_args['release_name']}"
+        return Response(
+            response=response_message, status=200, mimetype="application/json"
+        )
+    except Exception as e:
+        return Response(response=str(e), status=400, mimetype="application/json")
