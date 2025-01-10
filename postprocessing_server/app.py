c782971dfeb1f9c240cd0dba95bdbe2bf5bb8337
Agastya Mondal, PhD <amondal13@gmail.com>
2025-01-10 08:06:11 -0800

    Container app setup (#20)
    * flask + unicorn deps
    
    * flask and gunicorn boilerplate
    
    * uv Dockerfile + executables and caching
    
    * precommit
    
    * clean up folder structure
    
    * Switch from port 5000 -> 8000

---
+++ postprocessing_server/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def base():
+    """Root server route to ensure proper startup.
+    Returns:
+        str: Startup message.
+    """
+    return "<p>Rt postprocessing validation server running.</p>"
