f002a3a4508e123a75e0bbab93f66c105af85582
PatrickTCorbett <pyv3@cdc.gov>
2025-01-24 19:22:29 +0000

    adding timeseries plotting function using processed data from prior funtion

--- cfa_rt_postprocessing/plotting/timeseries.py
+++ cfa_rt_postprocessing/plotting/timeseries.py
@@ -195,3 +195,97 @@ def prepare_plot_data(date_to_use):
 obs_plot_data = prepare_plot_data(date_to_use=date_to_use)[0]
 interval_plot_data= prepare_plot_data(date_to_use=date_to_use)[1]
 
+
+###############################################
+# Plotting
+###############################################
+
+def timeseries_plot (date_to_use,
+                     state,
+                     pathogen,
+                     obs_plot_data,
+                     interval_plot_data):
+
+    import plotly.graph_objs as go
+    from plotly.subplots import make_subplots
+    import plotly.express as px #requires pandas
+
+    end_date = datetime.strptime(date_to_use, "%Y-%m-%d")
+    df_obs = (
+        obs_plot_data
+        .filter(pl.col("geo_value")==state,
+                pl.col('disease')==pathogen)
+        .select(["reference_date", "geo_value", "disease",  "processed_obs_data", "raw_obs_data","raw_obs_data_prev_wk","expected_obs_cases", "expected_nowcast_cases"])
+        .sort(['reference_date'])
+    )
+    df_interval = (
+        interval_plot_data
+        .filter(pl.col("geo_value")==state,
+                pl.col('disease')==pathogen)
+        .sort(['reference_date'])
+    )
+
+    # Line Traces
+    est_current_reported_trace = go.Scatter(x=df_obs['reference_date'], y=df_obs['expected_obs_cases'], mode='lines', name='Est. Current Reported', line_color="black")
+    est_total_reported_trace = go.Scatter(x=df_obs['reference_date'], y=df_obs['expected_nowcast_cases'], mode='lines', name='Est. Total Reported', line=dict(dash='dash'), line_color="black")
+    
+    # Marker Traces
+    processed_reported_trace = go.Scatter(x=df_obs['reference_date'], y=df_obs['processed_obs_data'], mode='markers', name='Processed Reported', marker=dict(symbol='circle'), visible='legendonly')
+    raw_reported_trace = go.Scatter(x=df_obs['reference_date'], y=df_obs['raw_obs_data'], mode='markers', name='Raw Reported', marker=dict(symbol='circle', color="black"))
+    raw_prev_wk_reported_trace = go.Scatter(x=df_obs['reference_date'], y=df_obs['raw_obs_data_prev_wk'], mode='markers', name='Raw Reported (Prev Week)', marker=dict(symbol='cross', color='rgba(255, 0, 0, 0.8)', size =5))
+
+    # Combine all traces into a list
+    traces = [est_current_reported_trace, est_total_reported_trace, processed_reported_trace, raw_reported_trace, raw_prev_wk_reported_trace]
+
+    # Create a figure with the collected traces
+    fig = go.Figure(data=traces)
+
+    # Loop through each width group to add ribbon traces
+    for group in df_interval['_width'].unique():
+        group_data = df_interval.filter(pl.col('_width') == group)
+        # Add trace for the ribbon area for each group
+        fig.add_trace(go.Scatter(
+            x=group_data['reference_date'].to_list() + group_data['reference_date'].to_list()[::-1],
+            y=group_data['_upper_expected_nowcast_cases'].to_list() + group_data['_lower_expected_nowcast_cases'].to_list()[::-1],
+            fill='toself',
+            fillcolor='rgba(75, 63, 63, 0.1)',
+            line=dict(color='rgba(151, 127, 105, 0.05)'),
+            name='Est. Total Interval: '+str(group)
+        ))
+    # Define background color according to pathogen
+    if pathogen=="COVID-19":
+        fig.update_layout(
+            #paper_bgcolor='rgba(255, 255, 255, 0.2)',  # Sets the color of the paper (area outside the plot)
+            plot_bgcolor='rgb(235, 229, 229)',       # Sets the color of the plot area
+            legend=dict(bgcolor='rgba(226, 225, 225, 0.6)',
+                bordercolor="Black",
+                borderwidth=1)
+        )
+    else:
+        fig.update_layout(
+            #paper_bgcolor='rgba(255, 255, 255, 0.2)',  # Sets the color of the paper (area outside the plot)
+            plot_bgcolor='rgba(107, 174, 214, 0.2)',       # Sets the color of the plot area
+            legend=dict(bgcolor='rgba(107, 174, 214, 0.1)',
+                bordercolor="Black",
+                borderwidth=1)
+        )
+
+    # Update the title
+    fig.update_layout(title=state + ": "+ pathogen + " ED Visits")
+    # Update the x-axis title
+    fig.update_xaxes(title_text="Reference Date")
+    # Update the y-axis title
+    fig.update_yaxes(title_text="Incident ED Visits")
+
+    # Save the figure as an HTML file
+    fig.write_html(state + "_"+ pathogen + "_" + end_date.strftime("%Y-%m-%d") + ".html")
+    # Show the plot
+    fig.show()
+
+timeseries_plot (date_to_use=date_to_use,
+                     state="CA",
+                     pathogen="COVID-19",
+                     obs_plot_data=obs_plot_data,
+                     interval_plot_data=interval_plot_data)
+
+
